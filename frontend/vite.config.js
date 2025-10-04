3bb9609abfa2bccf3f3175a82c946478e44bef1e
hakanaka61 <hakanaka@icloud.com>
2025-10-04 14:25:08 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './' // << bu çok önemli
+  base: './' // << çok önemli, relative path kullanır
 })
